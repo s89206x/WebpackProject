09afca340c9656fbc80b77971d0dc7871753a562
ice <smalltides@gmail.com>
2017-03-21 15:03:56 +0800

    code refactoring for load on demand

--- src/router.js
+++ src/router.js
@@ -3,11 +3,46 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+// import ArtistDetail from './components/artists/ArtistDetail';
+// import ArtistCreate from './components/artists/ArtistCreate';
+// import ArtistEdit from './components/artists/ArtistEdit';
+
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes: [
+    {
+      path: 'artists/new',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistCreate')
+          .then(module => cb(null, module.default))
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistDetail')
+          .then(module => cb(null, module.default))
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb) {
+        System.import('./components/artists/ArtistEdit')
+          .then(module => cb(null, module.default))
+      }
+    }
+  ]
+};
 
 const Routes = () => {
+  return (
+    <Router history={hashHistory} routes={componentRoutes} />
+  );
+};
+
+/*const Routes = () => {
   return (
     <Router history={hashHistory}>
       <Route path="/" component={Home}>
@@ -18,6 +53,6 @@ const Routes = () => {
       </Route>
     </Router>
   );
-};
+};*/
 
 export default Routes;
